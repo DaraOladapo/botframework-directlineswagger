213a65f66143a5f88a6d263ec7b5f47992783087
James Mann <james.mann@bcs.org>
2017-02-23 21:57:24 +0000

    initial checkin

---
+++ ConsoleApplication2/Bot Connector - Direct Line API - v3.0/BotConnectorDirectLineAPIV30AppServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Azure.AppService;
+
+namespace ConsoleApplication2
+{
+    public static class BotConnectorDirectLineAPIV30AppServiceExtensions
+    {
+        public static BotConnectorDirectLineAPIV30 CreateBotConnectorDirectLineAPIV30(this IAppServiceClient client)
+        {
+            return new BotConnectorDirectLineAPIV30(client.CreateHandler());
+        }
+
+        public static BotConnectorDirectLineAPIV30 CreateBotConnectorDirectLineAPIV30(this IAppServiceClient client, params DelegatingHandler[] handlers)
+        {
+            return new BotConnectorDirectLineAPIV30(client.CreateHandler(handlers));
+        }
+
+        public static BotConnectorDirectLineAPIV30 CreateBotConnectorDirectLineAPIV30(this IAppServiceClient client, Uri uri, params DelegatingHandler[] handlers)
+        {
+            return new BotConnectorDirectLineAPIV30(uri, client.CreateHandler(handlers));
+        }
+
+        public static BotConnectorDirectLineAPIV30 CreateBotConnectorDirectLineAPIV30(this IAppServiceClient client, HttpClientHandler rootHandler, params DelegatingHandler[] handlers)
+        {
+            return new BotConnectorDirectLineAPIV30(rootHandler, client.CreateHandler(handlers));
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication2.Models;
+using Microsoft.Rest;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BotConnectorDirectLineAPIV30 client = new BotConnectorDirectLineAPIV30(new TokenCredentials("<your direct line key here>"));
+            var conversationClient = new Conversations(client);
+
+            var result = conversationClient.StartConversationWithOperationResponseAsync().Result;
+            var waterMark = "";
+            while (true)
+            {
+                var textToSend = Console.ReadLine();
+                var r = conversationClient.PostActivityWithOperationResponseAsync(result.Body.ConversationId,
+                    new Activity() { Type = "message", From = new ChannelAccount() { ID = "James" }, Text = textToSend }).Result;
+
+                var activities = conversationClient.GetActivitiesWithOperationResponseAsync(result.Body.ConversationId, waterMark).Result;
+                waterMark = activities.Body.Watermark;
+
+                foreach (var activity in activities.Body.Activities)
+                {
+                    Console.WriteLine($"{activity.From.ID}:{activity.Text}");
+                }
+            }
+        }
+    }
+}
